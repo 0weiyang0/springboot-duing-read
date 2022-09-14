e6c75ef628aa1df81669fb0cc3ab606ef171b2fa
CarpeDiem <myCarpeDiem>
2022-09-14 21:56:03 +0800

    init

--- src/main/java/com/duing/controller/BookController.java
+++ src/main/java/com/duing/controller/BookController.java
@@ -3,12 +3,15 @@ package com.duing.controller;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.duing.model.Book;
 import com.duing.service.BookService;
+import com.duing.vo.BookVo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Controller
@@ -21,16 +24,25 @@ public class BookController {
         return "index";
     }
 
-
-
     @RequestMapping("/search")
-    @ResponseBody
-    public List<Book> search(@RequestParam("keyword") String keyword){
+//    @ResponseBody
+    public String search(@RequestParam("keyword") String keyword, Model model){
         System.out.println(keyword);
         QueryWrapper queryWrapper = new QueryWrapper();
         queryWrapper.like("name",keyword);
         List<Book> bookList = bookService.list();
-        System.out.println(bookList);
-        return bookList;
+        List<BookVo> bookVoList = new ArrayList<>();
+        for (Book book : bookList) {
+            BookVo vo = new BookVo();
+            vo.setName(book.getName());
+            vo.setAuthor(book.getAuthor());
+            vo.setDesc(book.getDescription());
+            vo.setImgPath(book.getId()+".png");
+            bookVoList.add(vo);
+        }
+//        System.out.println(bookList);
+        model.addAttribute("bookVoList",bookVoList);
+        System.out.println();
+        return "list";
     }
 }
---
+++ src/main/java/com/duing/vo/BookVo.java
@@ -0,0 +1,13 @@
+package com.duing.vo;
+
+import lombok.Data;
+
+@Data
+public class BookVo {
+
+    private String name;
+    private String author;
+    private String imgPath;
+    private String desc;
+
+}
